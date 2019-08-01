b0878420adf3b50f71453d536ef081c92e391d9c
Pavel <pavelp9415@gmail.com>
2019-08-01 23:47:43 +0300

    Added README.md, .gitignore. Completed unit 2.4

---
+++ README.md
@@ -0,0 +1,10 @@
+# Beginning C# Collections from https://www.pluralsight.com/
+
+------
+
+This repository aims to learn C# language and improving skills with the course by Simon Robinson (https://app.pluralsight.com/library/courses/csharp-collections-beginning/).
+
+## Contents
+
+- Module 1 - Course Overview
+- [Module 2 - Introducing Collections and Arrays](module_02)
---
+++ module_02/module_02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace module_02
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            foreach (string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
