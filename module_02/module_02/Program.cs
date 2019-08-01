34760adae97c1611c14f2fab9d6a1e1b8f317b58
Pavel <pavelp9415@gmail.com>
2019-08-02 00:35:40 +0300

    Completed 2nd module

--- module_02/module_02/Program.cs
+++ module_02/module_02/Program.cs
@@ -17,19 +17,27 @@ namespace module_02
                 "Sunday"
             };
 
-            /*
+            Console.WriteLine("Before:");
             foreach (string day in daysOfWeek)
             {
                 Console.WriteLine(day);
             }
-            */
+            
 
-            Console.WriteLine("Which day do you want to display");
+            Console.WriteLine("\nWhich day do you want to display");
             Console.WriteLine("(Monday = 1, etc.) > ");
             int iDay = int.Parse(Console.ReadLine());
 
             string choseDay = daysOfWeek[iDay - 1];
             Console.WriteLine($"That day is {choseDay}");
+
+            daysOfWeek[1] = "Wednesday";
+
+            Console.WriteLine("\r\nAfter:");
+            foreach (string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
         }
     }
 }
