392250e96bbb2bb7b54a3507c9a1192af423d548
Pavel <pavelp9415@gmail.com>
2019-08-03 21:49:23 +0300

    Completed unit 3.2

---
+++ module_03/module_03/CsvReader.cs
@@ -0,0 +1,17 @@
+﻿namespace module_03
+{
+    class CsvReader
+    {
+        private string _csvFilePath;
+
+        public CsvReader(string csvFilePath)
+        {
+            _csvFilePath = csvFilePath;
+        }
+
+        public Country[] ReadFirstNCountries(int nCountries)
+        {
+            return null;
+        }
+    }
+}
