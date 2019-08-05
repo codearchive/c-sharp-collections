e55619c8bb25104f5c37e0c94ba6eda5184844c7
Pavel <pavelp9415@gmail.com>
2019-08-05 23:52:30 +0300

    Completed unit 5.4

---
+++ module_05/module_05a/module_05a/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace module_05a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Country norway = new Country("Norway", "NOR", "Europe", 5_282_223);
+            Country finland = new Country("Finland", "FIN", "Europe", 5_511_303);
+
+            Dictionary<string, Country> countries = new Dictionary<string, Country>();
+            // var countries = new Dictionary<string, Country>();
+        }
+    }
+}
