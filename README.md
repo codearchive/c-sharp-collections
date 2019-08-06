c96a856599d0dc5564fbe67fdebf3a0bb66144fc
Pavel <pavelp9415@gmail.com>
2019-08-06 23:47:17 +0300

    Completed unit 7.2

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ This repository aims to learn C# language and improving skills with the course b
 - [Module 3 - Importing from a Data Source into an Array](module_03)
 - [Module 4 - Resizing Collections with Lists](module_04)
 - [Module 5 - Storing Keyed Data with Dictionaries](module_05)
+- [Module 6 - Manipulating List Data](module_06)
+- [Module 7 - Selecting Items Using LINQ](module_07)
---
+++ module_07/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace module_07
+{
+    class Program
+    {
+        static void Main()
+        {
+            string filePath = @"./Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            List<Country> countries = reader.ReadAllCountries();
+
+            foreach (Country country in countries.Take(10))
+            {
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+        }
+    }
+}
