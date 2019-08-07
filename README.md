9e0d01f88edaa2ef368331d3e57a3effe71e9a55
Pavel <pavelp9415@gmail.com>
2019-08-07 22:47:48 +0300

    Completed unit 8.5 - 8.8

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ This repository aims to learn C# language and improving skills with the course b
 - [Module 5 - Storing Keyed Data with Dictionaries](module_05)
 - [Module 6 - Manipulating List Data](module_06)
 - [Module 7 - Selecting Items Using LINQ](module_07)
+- [Module 8 - Creating Collections of Collections](module_08)
---
+++ module_08/module_08b/module_08b/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module_08b
+{
+    class Program
+    {
+        static void Main()
+        {
+            Game game = new Game();
+            game.PlayGame();
+            Console.WriteLine("Game over");
+        }
+    }
+}
