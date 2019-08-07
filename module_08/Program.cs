1716b4325001319a7dd8707cc9b623f566d7edfb
Pavel <pavelp9415@gmail.com>
2019-08-07 22:08:22 +0300

    Completed unit 8.4

--- module_08/Program.cs
+++ module_08/Program.cs
@@ -11,20 +11,26 @@ namespace module_08
             string filePath = @"./Pop by Largest Final.csv";
             CsvReader reader = new CsvReader(filePath);
 
-            List<Country> countries = reader.ReadAllCountries();
-
-            var filteredCountries = countries.Where(x => !x.Name.Contains(',')); //.Take(20);
-            var filteredCountries2 = from country in countries
-                where !country.Name.Contains(',')
-                select country;
-
-            foreach (Country country in filteredCountries)
+            Dictionary<string, List<Country>> countries = reader.ReadAllCountries();
+            foreach (var region in countries.Keys)
             {
-                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+                Console.WriteLine(region);
             }
 
-            for (int i = 12; i <= 14; i++)
-                Console.WriteLine(countries[i].Name);
+            Console.WriteLine("Which of the above regions do you want? ");
+            string chosenRegion = Console.ReadLine();
+
+            if (countries.ContainsKey(chosenRegion))
+            {
+                foreach (Country country in countries[chosenRegion].Take(10))
+                {
+                    Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid region");
+            }
         }
     }
 }
